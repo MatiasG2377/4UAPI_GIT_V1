38da0f5bb675a238a188d77aa96619b245ed40d2
Matías Gamboa <matiasg002@outlook.com>
2024-05-09 10:22:45 -0500

    Corrección
    Corrección de los atributos al hacer get de un usuario.

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ async def read_user(user_id: int, db: Session = db_dependency):
     if user is None:
         raise HTTPException(status_code=404, detail='User not found')
     # Convertir las fechas a strings antes de devolverlas en la respuesta
-    return {**user.__dict__, "user_date": user.user_date.isoformat(), "user_birth_date": user.user_birth_date.isoformat()}
+    return {**user.__dict__, "user_date": user.user_date.isoformat()}
 
 @app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int, db: Session = db_dependency):
