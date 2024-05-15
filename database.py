5bd58fa922ca709d43fd1f62551bf1e023070d74
Matías Gamboa <matiasg002@outlook.com>
2024-05-15 11:18:47 -0500

    Cambio de BD
    Nuevas credenciales

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DB_USER = 'uglippajbziyfn7z'
-DB_PASSWORD = 'tz9BH7MNWU84RsGME1px'
-DB_HOST = 'bdcdv72tkuakz9qbnlc3-mysql.services.clever-cloud.com'
+DB_USER = 'uwkroqoqou2ieroj'
+DB_PASSWORD = '6MVRtb8rvM3HQ9NMPJ47'
+DB_HOST = 'b38upoqwbjpslbwjl572-mysql.services.clever-cloud.com'
 DB_PORT = '3306'
-DB_NAME = 'bdcdv72tkuakz9qbnlc3'
+DB_NAME = 'b38upoqwbjpslbwjl572'
 
 # URL de conexión a la base de datos en la nube
 URL_DATABASE = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
